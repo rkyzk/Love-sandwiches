19b43c04f01c10953c13422d2dccbe460ff277f5
rkyzk <reikoyazaki@yahoo.com>
2022-11-19 10:52:37 +0000

    revise validation(incomplete)

--- run.py
+++ run.py
@@ -11,6 +11,22 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+        
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    Check if the input contains 6 integers, and store the data.
+    """
+    while True:
+        print("Enter sales data (For example, '35,40,41,35,46,30')")
+        data = input("Your input: ")
+        list = data.split(',')
+        print(f"You entered {list}")
+        sales_data = validate_data(list)
+        if sales_data:
+            print("The data has been stored")
+            break 
+    return sales_data
 
 def validate_data(values):
     """
@@ -28,18 +44,6 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid entry: {e}.  Please try again")
-        
-def get_sales_data():
-    """
-    Get sales figures input from the user
-    Check if the input contains 6 integers, and store the data.
-    """
-    
-    print("Enter sales data (For example, '35,40,41,35,46,30')")
-    data = input("Your input: ")
-    list = data.split(',')
-    print(f"You entered {list}")
-    validate_data(list)
 
-get_sales_data()
-
+data = get_sales_data()
+print(data)
