337f3cf926a7d72bc72d121104deca922fe476a8
rkyzk <reikoyazaki@yahoo.com>
2022-11-19 05:50:29 +0000

    set up collecting sales data

--- run.py
+++ run.py
@@ -10,10 +10,35 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love-sandwiches')
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    """
+    Get sales figures input from the user 
+    """
+    print("Enter sales data (For example, '35, 40, 41, 35, 46, 30')")
+    data = input("Your input: ")
+    print(f"You enetered: {data}")
+    validation = input("Enter 'v' to validate, or 'r' to re-enter: ")
+    list = []
+    if validation != 'v' and validation != 'r':
+        print("Invalid entry.")
+        continue
+    if validation == 'r':
+        continue
 
-data = sales.get_all_values()
+    list = data.split(', ')
+    if not len(list) == 6:
+        print("Enter 6 values.")    
+        continue
 
-print(data)
+    for num in list:
+        if not num.isdigit():
+            print("Enter only numbers.")        
+            
+    list = [int(n) for n in list]
+    print("The data have been stored.")
+    
+
+    
+        
